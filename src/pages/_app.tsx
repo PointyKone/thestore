d89109d80855ac2b41776ddcc707b16e68072bf1
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-06-18 18:03:07 +1000

    feat: :card_file_box: added tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,39 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Layout from './components/layout'
+import { AppType } from 'next/dist/shared/lib/utils'
+import { withTRPC } from '@trpc/next'
+import type { AppRouter } from '../backend/router'
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  // Use the layout defined at the page level, if available
-
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   )
 }
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { ReactElement } from 'react'
-import Layout from './components/layout'
-import Navbar from './components/navbar'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { ReactElement } from "react";
+import { trpc } from "../utils/trpc";
+import Layout from "./components/layout";
+import Navbar from "./components/navbar";
 
-const Home: NextPage = () => {
+export default function Home() {
+	const { data, isLoading } = trpc.useQuery(["hello", { text: "Kian" }]);
+
+	if(isLoading) return <div>Loading...</div>;
+
+	if(data) return <div>{data.greeting}</div>
+
 	return (
 		<div className="">
-			<Navbar />
+		<Navbar />
 		</div>
-	)
+	);
 }
-export default Home
